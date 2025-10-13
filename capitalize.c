89264de6f8db7b5c2f341795468d80bcb12488ef
ismailatibatwit <144959357+ismailatibatwit@users.noreply.github.com>
2025-10-12 22:27:28 -0400

    Update capitalize.c

--- capitalize.c
+++ capitalize.c
@@ -1,10 +1,30 @@
-#include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * TODO: Describe what the function does
+ * creates a new string with all lowercase letters converted to uppercase 
+ * returns a string of dynamic allocation that needs to be freedsd
  */
-char* capitalize(const char* s) {
-    return NULL;
+char* capitalize(const char *s) {
+    if (s == NULL) {
+        return NULL;
+    }
+    
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+    char *result = (char*)malloc((len + 1) * sizeof(char));
+    if (result == NULL) {
+        return NULL;
+    }
+    for (int i = 0; i < len; i++) {
+        result[i] = toupper(s[i]);
+    }
+    
+    // add null terminator
+    result[len] = '\0';
+    
+    return result;
 }
-
